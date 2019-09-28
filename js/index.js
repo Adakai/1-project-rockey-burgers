5d42850b4ecd80b9309c1c5fa8a7152da784a9eb
Adakai <abovethenegativity23@gmail.com>
2019-09-28 00:40:00 -0600

    added js folder and file, finish careers page,also made FAQ page and completed it.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function myFunctionOne() {
+    var x = document.getElementById("hide_show_one");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function myFunctionTwo() {
+    var x = document.getElementById("hide_show_two");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
